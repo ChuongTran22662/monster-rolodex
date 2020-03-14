797e77770c3ac18a700845d4d262168491f21111
Tran Dao Quang Chuong <chuongcongtu9x@gmail.com>
2020-03-14 21:43:07 +0700

    Updates

---
+++ precache-manifest.ca4319bf338ef8450238ded3bfeac9e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1943c4a74c79dd8da5e26d040c69fb8b",
+    "url": "/ChuongTran22662/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1fd2d213d5ce38917ead",
+    "url": "/ChuongTran22662/monster-rolodex/static/css/main.5c3104f8.chunk.css"
+  },
+  {
+    "revision": "7fe0845a347327439809",
+    "url": "/ChuongTran22662/monster-rolodex/static/js/2.80027728.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ChuongTran22662/monster-rolodex/static/js/2.80027728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fd2d213d5ce38917ead",
+    "url": "/ChuongTran22662/monster-rolodex/static/js/main.240a605d.chunk.js"
+  },
+  {
+    "revision": "496f2a588f48509cadbc",
+    "url": "/ChuongTran22662/monster-rolodex/static/js/runtime-main.ce6e6bc3.js"
+  }
+]);
